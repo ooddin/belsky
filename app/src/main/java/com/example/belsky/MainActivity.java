947c7c0bf1cc286c5dbb1043c0baa1569c8a8a5a
ooddin <ooddinenok123@mail.ru>
2022-04-02 11:50:13 +0300

    first commit

---
+++ app/src/main/java/com/example/belsky/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.belsky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int ps4=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,MainActivityweb.class));
+                finish();
+            }
+        },ps4);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/belsky/MainActivityweb.java
@@ -0,0 +1,14 @@
+package com.example.belsky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityweb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activityweb);
+    }
+}
